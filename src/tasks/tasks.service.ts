4203c00e2d07ddefc78b7611c7b43b4d5aae148d
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-08-03 16:38:20 -0600

    🎨 32. Implement the `NotFoundException`

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto.class';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -31,7 +31,13 @@ export class TasksService {
   }
 
   getTask(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+
+    return task;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
