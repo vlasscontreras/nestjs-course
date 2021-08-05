26faa22b727b8b1445b3caba11f4a2c03c51a65a
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-08-05 14:29:52 -0600

    ✨ 58. Add the signup controller, service, and repository methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
@@ -7,4 +8,8 @@ export class AuthService {
   constructor(
     @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
