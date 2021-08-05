ca446bac9ef39ee3b0454fd1341895e5c308433e
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-08-05 15:36:05 -0600

    🚚 63. Add the sign in controller and service methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,5 +12,16 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ username });
+
+    if (user && (await compare(password, user.password))) {
+      return 'success';
+    }
+
+    throw new UnauthorizedException('invalid credentials');
   }
 }
