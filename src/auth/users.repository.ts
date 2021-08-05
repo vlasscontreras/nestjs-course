1260c752ffa4a243e49586801213c25ccef8f678
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-08-05 15:29:59 -0600

    🔐 62. Hash passwords when creating users

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -3,6 +3,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
@@ -11,9 +12,13 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    // Hashing the password
+    const salt = await bcrypt.genSaltSync();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username: username,
-      password: password,
+      password: hashedPassword,
     });
 
     try {
