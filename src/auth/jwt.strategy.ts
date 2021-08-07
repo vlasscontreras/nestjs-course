82cec6bdb91a0020a8e43c96cc9fb856e529f519
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-08-07 08:51:34 -0600

    🎨 90. Update JWT secret config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from './jwt.payload.interface';
@@ -9,9 +10,10 @@ import { UsersRepository } from './users.repository';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @Inject(UsersRepository) private usersRepository: UsersRepository,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'secret',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -5,9 +5,13 @@ import {
 } from '@hapi/joi';
 
 export const configValidationSchema = configObject({
+  // Database.
   DB_HOST: configString().required(),
   DB_PORT: configNumber().default(3306).required(),
   DB_NAME: configString().required(),
   DB_USERNAME: configString().required(),
   DB_PASSWORD: configString().required(),
+
+  // JWT.
+  JWT_SECRET: configString().required(),
 });
