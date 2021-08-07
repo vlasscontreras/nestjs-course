7ffdbbd6c67d2924bd59fed7011329076671d077
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-08-07 08:12:58 -0600

    🔧 87. Set up NestJS config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnectionOptions } from 'typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: async () =>
         Object.assign(await getConnectionOptions(), {
